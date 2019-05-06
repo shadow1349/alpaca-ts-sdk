fcdf05f7afe5b33983622269b2e79157db53f467
Sam Redmond <sredmond@techusolutions.com>
2019-05-06 16:04:54 -0500

    Cleaning up classes

--- src/classes/MarketData/MarketData.ts
+++ src/classes/MarketData/MarketData.ts
@@ -1,25 +1,16 @@
 import { AlpacaOptions } from '../AlpacaOptions';
 import { Reference } from './Reference';
-import { Exchanges, HistoricQuote, HistoricTrades, DailyOpenClose, LastReference } from './Stocks';
+import { Stocks } from './Stocks';
 import { Symbol } from './Symbol';
 
 export class MarketData {
   Symbol: Symbol;
   Reference: Reference;
-  Exchanges: Exchanges;
-  HistoricQuotes: HistoricQuote;
-  HistoricTrades: HistoricTrades;
-  MarketOpenClose: DailyOpenClose;
-  LastTradeQuote: LastReference;
+  Stocks: Stocks;
 
   constructor(private options: AlpacaOptions) {
     this.Reference = new Reference(this.options);
-    
-    this.Symbol = new Symbol(this.options); 
-    this.Exchanges = new Exchanges(this.options);
-    this.HistoricQuotes = new HistoricQuote(this.options);
-    this.HistoricTrades = new HistoricTrades(this.options);
-    this.MarketOpenClose = new DailyOpenClose(this.options);
-    this.LastTradeQuote = new LastReference(this.options);
+    this.Symbol = new Symbol(this.options);
+    this.Stocks = new Stocks(this.options);
   }
 }
--- src/classes/MarketData/Stocks/index.ts
+++ src/classes/MarketData/Stocks/index.ts
@@ -3,3 +3,4 @@ export * from './Exchanges';
 export * from './HistoricQuotes';
 export * from './HistoricTrades';
 export * from './LastReference';
+export * from './Stocks';
